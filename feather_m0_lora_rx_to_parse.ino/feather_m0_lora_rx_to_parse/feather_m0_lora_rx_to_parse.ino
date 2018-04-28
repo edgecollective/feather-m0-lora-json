a53fb502bff2345b858169bc2466a145fc7047b4
DB <dwblair@localhost.localdomain>
2018-04-28 13:06:34 -0400

    update

--- feather_m0_lora_rx_to_parse.ino/feather_m0_lora_rx_to_parse/feather_m0_lora_rx_to_parse.ino
+++ feather_m0_lora_rx_to_parse.ino/feather_m0_lora_rx_to_parse/feather_m0_lora_rx_to_parse.ino
@@ -67,7 +67,7 @@
 
 
 // Change to 434.0 or other frequency, must match RX's freq!
-// #define RF95_FREQ 915.0
+//#define RF95_FREQ 915.0
 #define RF95_FREQ 868.0
 
 // Singleton instance of the radio driver
@@ -88,14 +88,12 @@ void setup()
   digitalWrite(RFM95_RST, HIGH);
 
   Serial.begin(115200);
-  /*
-  while (!Serial) {
-    delay(1);
-  }
-  */
+ // while (!Serial) {
+  //  delay(1);
+ // }
   delay(100);
 
-  Serial.println("Feather LoRa RX Test!");
+ // Serial.println("Feather LoRa RX Test!");
 
   // manual reset
   digitalWrite(RFM95_RST, LOW);
@@ -107,14 +105,14 @@ void setup()
     Serial.println("LoRa radio init failed");
     while (1);
   }
-  Serial.println("LoRa radio init OK!");
+  //Serial.println("LoRa radio init OK!");
 
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
+   // Serial.println("setFrequency failed");
     while (1);
   }
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  //Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
 
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
@@ -135,46 +133,31 @@ void loop()
     if (rf95.recv(buf, &len))
     {
       digitalWrite(LED, HIGH);
-      RH_RF95::printBuffer("Received: ", buf, len);
-      Serial.print("Got: ");
+      //RH_RF95::printBuffer("Received: ", buf, len);
+    //  Serial.print("Got: ");
       Serial.println((char*)buf);
-
-
-  // Fetch values.
-  //
-  // Most of the time, you can rely on the implicit casts.
-  // In other case, you can do root["time"].as<long>();
-
-  /*
-  const char* sensor = root["sensor"];
-  long time = root["time"];
-  double latitude = root["data"][0];
-  double longitude = root["data"][1];
-
-  // Print values.
-  Serial.println(sensor);
-  Serial.println(time);
-  Serial.println(latitude, 6);
-  Serial.println(longitude, 6);
-*/
 
 
 
 
       
-       Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
+       //Serial.print("RSSI: ");
+     // Serial.println(rf95.lastRssi(), DEC);
 
       // Send a reply
       uint8_t data[] = "And hello back to you";
       rf95.send(data, sizeof(data));
       rf95.waitPacketSent();
-      Serial.println("Sent a reply");
+     // Serial.println("Sent a reply");
       digitalWrite(LED, LOW);
     }
     else
     {
-      Serial.println("Receive failed");
+    //
+    
+    
+    
+  Serial.println("Receive failed");
     }
   }
 }
